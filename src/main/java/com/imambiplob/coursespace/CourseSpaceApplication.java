af23b5d5744b501d35aaf5029dce4760200f119c
ImamBiplob <imamhbiplob@gmail.com>
2023-08-22 17:39:20 +0600

    first-commit

---
+++ src/main/java/com/imambiplob/coursespace/CourseSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.imambiplob.coursespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imambiplob/coursespace/CourseSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imambiplob.coursespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
